d1a284f8543858a44c8274d2b154584b4ea54a82
AhmedMohamed50 <85414834+AhmedMohamed50@users.noreply.github.com>
2024-08-12 12:01:18 +0300

    day 5

---
+++ day5/js/script.js
@@ -0,0 +1,19 @@
+
+
+// functions
+
+// 1. function statement 
+
+// function add (number1, number2) {
+//     return number1 + number2;
+// }
+
+// var result = add(5,3);
+// console.log(result)
+
+// var result = add(5);
+// console.log(result) // NaN
+
+// var result = add();  // NaN
+// console.log(result);
+
