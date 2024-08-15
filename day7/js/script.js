0f5c15e66ba8651e97657daeb7d94edb88fae4d4
AhmedMohamed50 <85414834+AhmedMohamed50@users.noreply.github.com>
2024-08-15 17:53:34 +0300

    modifications

--- day7/js/script.js
+++ day7/js/script.js
@@ -42,13 +42,14 @@ var newWindow;
 function openNew(){
     newWindow = window.open("../day7/small-window.html", "text win", "width=300, height=300");
     console.log(newWindow.closed);
-
-    if (newWindow) {
-        var myDiv = document.createElement("div");
-        var myName = document.createTextNode("my name is Ahmed Mohamed");
-        myDiv.appendChild(myName);
-        console.log(myDiv); // working correctly
-        newWindow.document.body.appendChild(myDiv);
+    if(newWindow) {
+        myWindow.onload = function() {
+            var myDiv = document.createElement("div");
+            var myName = document.createTextNode("my name is Ahmed Mohamed");
+            myDiv.appendChild(myName);
+            console.log(myDiv); // working correctly
+            newWindow.document.body.appendChild(myDiv);
+        };
     }
 }
 
@@ -63,42 +64,42 @@ setInterval(function(){
 
 
 // 4- create img slider that changes the pictures every 4 seconds or when user presses button next/prev
-// var currentIndex = 0;
-// var images = document.querySelectorAll('.slides img');
-// var totalImages = images.length;
+var currentIndex = 0;
+var images = document.querySelectorAll('.slides img');
+var totalImages = images.length;
 
-// function showImage(index) {
-//     images[currentIndex].classList.remove('active');
-//     currentIndex = (index + totalImages) % totalImages;
-//     images[currentIndex].classList.add('active');
-// }
+function showImage(index) {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (index + totalImages) % totalImages;
+    images[currentIndex].classList.add('active');
+}
 
-// function nextImage() {
-//     showImage(currentIndex + 1);
-// }
+function nextImage() {
+    showImage(currentIndex + 1);
+}
 
-// function prevImage() {
-//     showImage(currentIndex - 1);
-// }
+function prevImage() {
+    showImage(currentIndex - 1);
+}
 
-// // Automatic slide change every 4 seconds
-// var autoSlide = setInterval(nextImage, 4000);
+// Automatic slide change every 4 seconds
+var autoSlide = setInterval(nextImage, 4000);
 
-// // Stop the auto slide when the user interacts with the slider
-// function stopAutoSlide() {
-//     clearInterval(autoSlide);
-// }
+// Stop the auto slide when the user interacts with the slider
+function stopAutoSlide() {
+    clearInterval(autoSlide);
+}
 
-// var prevButton = document.getElementsByClassName("prev")[0];
-// console.log(prevButton)
-// prevButton.onclick = function() {
-//     stopAutoSlide();
-//     prevImage();
-// }
+var prevButton = document.getElementsByClassName("prev")[0];
+console.log(prevButton)
+prevButton.onclick = function() {
+    stopAutoSlide();
+    prevImage();
+}
 
-// var nextButton = document.getElementsByClassName("next")[0];
-// console.log(nextButton)
-// nextButton.onclick = function() {
-//     stopAutoSlide();
-//     nextImage();
-// }
+var nextButton = document.getElementsByClassName("next")[0];
+console.log(nextButton)
+nextButton.onclick = function() {
+    stopAutoSlide();
+    nextImage();
+}
