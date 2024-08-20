55b0845d76806aa221312db24b14586cff033e29
AhmedMohamed50 <85414834+AhmedMohamed50@users.noreply.github.com>
2024-08-20 09:40:24 +0300

    modification of files frontend & backend & database

--- Frontend/day8/js/script.js
+++ Frontend/day8/js/script.js
@@ -1,21 +1,3 @@
-// function fetchData(isRejected) {
-//     return new Promise((resolve, reject)=> {
-//         setTimeout(() => {
-//             if(isRejected) {
-//                 reject('Rejected');
-//             }
-//             else {
-//                 let date = {name1: "ahmed", name2: "mohamed"};
-//                 resolve(date);
-//             }
-//     },3000)
-//     })
-// }
-
-// fetchData(false)
-// .then(data => console.log(data))
-// .catch(err => console.error(err))
-
 
 // 1.Change the text of a button with the ID "myButton" to "Clicked!" when it is clicked.
 document.getElementById("myButton").addEventListener("click", function() {
