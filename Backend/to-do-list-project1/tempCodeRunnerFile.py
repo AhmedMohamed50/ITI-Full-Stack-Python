a2baf677992f5fa49fb74036d14e235d27540a1d
AhmedMohamed50 <85414834+AhmedMohamed50@users.noreply.github.com>
2024-08-23 22:32:43 +0300

    to do list

---
+++ Backend/to-do-list-project1/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+# import main                                 # method 1
+# from main import view_tasks, add_task       # method 2
+from main import *
+
+new_tasks = [{"task":"Quran", "completed":True}, {"task":"Salah", "completed":True}, 
+    {"task":"Study", "completed":False}, {"task":"Exercise", "completed":True}, 
+    {"task":"Sleep", "completed":False}, {"task":"Visit Hamada", "completed":True}]
+
+# main.view_tasks(new_tasks)                  # method 1
+view_tasks(new_tasks)                         # method 2 and 3
+
+print(__name__) # __main__ => now i am running this file directly
